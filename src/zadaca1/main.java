6ebb75bf23ef760dcf1686a27fc12a32318cd43f
marijaT13 <tashevska254@gmail.com>
2022-12-01 22:47:06 +0100

    zadaca1

---
+++ src/zadaca1/main.java
@@ -0,0 +1,18 @@
+package zadaca1;
+
+import java.util.ArrayList;
+import java.util.List;
+public class main {
+
+	public static void main(String[] args) {
+		List<String> gradovi = new ArrayList<String>();
+		gradovi.add("Ohrid");
+		gradovi.add("Bitola");
+		gradovi.add("Skopje");
+		gradovi.add("Prilep");
+		gradovi.add("Tetovo");
+		
+		System.out.println(gradovi);
+	}
+
+}
