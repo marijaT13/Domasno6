d34a58ea9339487156bc5246eec918dee701b411
marijaT13 <tashevska254@gmail.com>
2022-12-01 22:47:29 +0100

    zadaca5

---
+++ src/zadaca5/main.java
@@ -0,0 +1,18 @@
+package zadaca5;
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+
+	public static void main(String[] args) {
+		List<Integer> broevi= new ArrayList<Integer>();
+		broevi.add(8);
+		broevi.add(21);
+		broevi.add(22);
+		broevi.add(13);
+		broevi.add(11);
+		
+		System.out.println("Vtoriot element e: "+broevi.get(1));
+		}
+
+}
